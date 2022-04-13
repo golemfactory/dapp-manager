b5606232de8ededeca4f73afef58f363600cb459
Jan Betley <jan.betley@golem.network>
2022-04-13 15:47:55 +0200

    black auto-styling

--- tests/test_manager_api.py
+++ tests/test_manager_api.py
@@ -1,4 +1,4 @@
-'''A bunch of E2E tests of the DappManager interface'''
+"""A bunch of E2E tests of the DappManager interface"""
 import psutil
 import pytest
 from time import sleep
@@ -9,7 +9,7 @@ from .helpers import start_dapp, process_is_running, get_dapp_scenarios, asset_p
 
 
 def test_start():
-    command = ['sleep', '1']
+    command = ["sleep", "1"]
     dapp = start_dapp(command)
 
     pid = dapp.pid
@@ -20,16 +20,18 @@ def test_start():
 
 
 def test_list():
-    dapp_1 = start_dapp(['echo', 'foo'])
-    sleep(0.01)  # ensure second dapp is created after the first (we test the app_id order)
-    dapp_2 = start_dapp(['echo', 'bar'])
+    dapp_1 = start_dapp(["echo", "foo"])
+    sleep(
+        0.01
+    )  # ensure second dapp is created after the first (we test the app_id order)
+    dapp_2 = start_dapp(["echo", "bar"])
 
     assert DappManager.list() == [dapp_1.app_id, dapp_2.app_id]
 
 
-@pytest.mark.parametrize('get_dapp', get_dapp_scenarios)
+@pytest.mark.parametrize("get_dapp", get_dapp_scenarios)
 def test_stop(get_dapp):
-    dapp = get_dapp(['sleep', '3'])
+    dapp = get_dapp(["sleep", "3"])
     assert process_is_running(dapp.pid)
 
     #   NOTE: `stop` is not guaranted to succeed for every process (because it only SIGINTs),
@@ -39,9 +41,9 @@ def test_stop(get_dapp):
     assert not process_is_running(dapp.pid)
 
 
-@pytest.mark.parametrize('get_dapp', get_dapp_scenarios)
+@pytest.mark.parametrize("get_dapp", get_dapp_scenarios)
 def test_stop_timeout_kill(get_dapp):
-    dapp = get_dapp([asset_path('sleep_no_sigint.sh'), '10'])
+    dapp = get_dapp([asset_path("sleep_no_sigint.sh"), "10"])
     sleep(0.01)
 
     #   stop times out because command ignores sigint
@@ -54,13 +56,15 @@ def test_stop_timeout_kill(get_dapp):
     assert not process_is_running(dapp.pid)
 
 
-@pytest.mark.parametrize('get_dapp', get_dapp_scenarios)
+@pytest.mark.parametrize("get_dapp", get_dapp_scenarios)
 def test_raw_status_raw_data(get_dapp):
-    dapp = get_dapp([asset_path('write_mock_files.sh')], status_file=True, data_file=True)
+    dapp = get_dapp(
+        [asset_path("write_mock_files.sh")], status_file=True, data_file=True
+    )
     sleep(0.01)
 
-    with open(asset_path('mock_status_file.txt')) as f:
+    with open(asset_path("mock_status_file.txt")) as f:
         assert dapp.raw_status() == f.read()
 
-    with open(asset_path('mock_data_file.txt')) as f:
+    with open(asset_path("mock_data_file.txt")) as f:
         assert dapp.raw_data() == f.read()
