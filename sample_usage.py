85dbd80a0f13c41cb164f6d49b8fbe70b3f46d73
Jan Betley <jan.betley@golem.network>
2022-05-02 16:25:04 +0200

    Fix in the sample_usage (this file is redundant either way)

--- sample_usage.py
+++ sample_usage.py
@@ -2,14 +2,14 @@ from yagna_dapp_manager import DappManager
 
 #   New app
 dapp = DappManager.start("golem_compose.yml", config="config_file.yml")
-print(dapp.raw_data())
-print(dapp.raw_state())
+print(dapp.raw_data(ensure_alive=True))
+print(dapp.raw_state(ensure_alive=True))
 
 #   Old app
 app_id = DappManager.list()[0]
 dapp = DappManager(app_id)
-print(dapp.raw_data())
-print(dapp.raw_state())
+print(dapp.raw_data(ensure_alive=True))
+print(dapp.raw_state(ensure_alive=True))
 
 #   Stop the app
 app_id = DappManager.list()[0]
