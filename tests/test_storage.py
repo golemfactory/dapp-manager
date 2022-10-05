5903cceffba1b469b384b76eb3ded2629d95b28c
shadeofblue <blue@golem.network>
2022-10-05 09:06:27 +0200

    prevent the `app-id` from traversing outside of the designated data directory

---
+++ tests/test_storage.py
@@ -0,0 +1,21 @@
+import pytest
+
+from dapp_manager.exceptions import UnknownApp
+from dapp_manager.storage import SimpleStorage
+
+
+def test_storage_data_dir():
+    storage = SimpleStorage("foo", "/bar")
+    assert str(storage._data_dir.resolve()) == "/bar/foo"
+
+
+def test_storage_app_id_special_chars():
+    storage = SimpleStorage("..foo", "/bar")
+    assert str(storage._data_dir.resolve()) == "/bar/foo"
+
+
+def test_storage_app_id_disallows_traversal():
+    storage = SimpleStorage("", "/bar")
+    storage.app_id = "../foo"
+    with pytest.raises(UnknownApp):
+        storage._data_dir.resolve()
